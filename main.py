cf709d640c784e7cf5fff99f6c51268f35201451
81493972 <adityad39c@gmail.com>
2024-12-12 15:22:01 -0800

    delete lines for old apis in main.py

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ from api.group import group_api
 from api.section import section_api
 from api.nestPost import nestPost_api # Justin added this, custom format for his website
 from api.messages_api import messages_api # Adi added this, messages for his website
-from api.carphoto import car_api
-from api.carChat import car_chat_api
 
 from api.teaminfo import teaminfo_api
 
@@ -49,12 +47,10 @@ app.register_blueprint(post_api)
 app.register_blueprint(channel_api)
 app.register_blueprint(group_api)
 app.register_blueprint(section_api)
-app.register_blueprint(car_chat_api)
 # Added new files to create nestPosts, uses a different format than Mortensen and didn't want to touch his junk
 app.register_blueprint(nestPost_api)
 app.register_blueprint(nestImg_api)
 app.register_blueprint(vote_api)
-app.register_blueprint(car_api)
 app.register_blueprint(teaminfo_api)
 # Tell Flask-Login the view function name of your login route
 login_manager.login_view = "login"
