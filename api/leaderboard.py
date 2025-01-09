6612632ea03931bd570677bde405198b3ca578d2
SanPranav <pranavs22638@gmail.com>
2025-01-09 00:07:55 -0800

    applying the advice i got from college students (adding comments to every line)

--- api/leaderboard.py
+++ api/leaderboard.py
@@ -14,32 +14,47 @@ class Leaderboard:
     class _TopUsers(Resource):
         def get(self):
             try:
-                # Get the current user
+                # Get the current user ID from the request arguments
                 current_user_id = request.args.get('user_id')
+                
+                # Query the database to get the current user by ID
                 current_user = User.query.get(current_user_id)
+                
+                # If the current user is not found, return a 404 error message
                 if not current_user:
                     return {'message': 'User not found'}, 404
                 
+                # Split the current user's interests into a set of interests
                 current_user_interests = set(current_user.interests.split(", "))
                 
-                # Get all users except the current user
+                # Query the database to get all users except the current user
                 all_users = User.query.filter(User.id != current_user_id).all()
                 
                 matched_users = []
+                # Iterate over all users
                 for user in all_users:
+                    # Split each user's interests into a set of interests
                     user_interests = set(user.interests.split(", "))
+                    
+                    # Find the shared interests between the current user and each user
                     shared_interests = current_user_interests.intersection(user_interests)
+                    
+                    # If there are shared interests, add the user to the matched users list
                     if shared_interests:
                         matched_users.append({
                             'username': user.name,
                             'shared_interests': list(shared_interests)
                         })
                 
-                # Sort matched users by the number of shared interests (descending)
+                # Sort the matched users by the number of shared interests in descending order
+                # The key=lambda x: len(x['shared_interests']) part specifies that the sorting key is the length of the 'shared_interests' list
+                # reverse=True means that the list will be sorted in descending order
                 matched_users.sort(key=lambda x: len(x['shared_interests']), reverse=True)
                 
+                # Return the matched users as a JSON response
                 return jsonify({'top_users': matched_users})
             except Exception as e:
+                # If there is an error, return a 500 error message
                 return {'message': f'Error retrieving top users: {str(e)}'}, 500
 
     # Create class for top interests
@@ -50,20 +65,27 @@ class Leaderboard:
                 all_users = User.query.all()
                 
                 interest_counts = {}
+                # Iterate over all users
                 for user in all_users:
+                    # Split each user's interests into a list of interests
                     interests = user.interests.split(", ")
+                    
+                    # Count the occurrences of each interest
                     for interest in interests:
                         if interest in interest_counts:
                             interest_counts[interest] += 1
                         else:
                             interest_counts[interest] = 1
                 
-                # Sort interests by count
+                # Sort interests by count in descending order
+                # The key=lambda x: x[1] part specifies that the sorting key is the count of each interest
+                # reverse=True means that the list will be sorted in descending order
                 sorted_interests = sorted(interest_counts.items(), key=lambda x: x[1], reverse=True)
                 
-                # Return the top interests
+                # Return the top interests as a JSON response
                 return jsonify({'top_interests': [{'interest': interest, 'count': count} for interest, count in sorted_interests]})
             except Exception as e:
+                # If there is an error, return a 500 error message
                 return {'message': f'Error retrieving top interests: {str(e)}'}, 500
 
 # Add resources to the API
