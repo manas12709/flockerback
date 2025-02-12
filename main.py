b22922df8744a739095169d2d482fee226d83064
Hypernova101 <mihirnbapat@gmail.com>
2025-02-11 18:30:41 -0800

    vote shows up on backend management too

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ from api.chat import chat_api
 from api.language import language_api
 from api.interests import interests_api  # Import the new interests API
 from api.chat_met import chat_met_api
+from api.vote_met import vote_met_api
 
 from api.leaderboard import leaderboard_api
 
@@ -68,6 +69,7 @@ app.register_blueprint(player_api)
 app.register_blueprint(language_api)
 app.register_blueprint(interests_api)
 app.register_blueprint(chat_met_api)
+app.register_blueprint(vote_met_api)
 
 # Tell Flask-Login the view function name of your login route
 login_manager.login_view = "login"
